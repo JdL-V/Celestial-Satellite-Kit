43c3f33dfcb2ceb653bd7794fcd99c5ccd12683d
Jqazwsx <jmjorgem11@gmail.com>
2022-06-20 01:33:11 +0200

    adcs_select

--- Orbit_ADCS/main.m
+++ Orbit_ADCS/main.m
@@ -15,7 +15,6 @@ addpath(genpath(fileparts(which(mfilename))));
 n = 20;                   % number of orbits (15)
 dT = 5;                   % time step (0.5)
 case_adcs = 1;            % case ADCS (1=sigue las lineas de campo, 2=x mirando a nadir, 3=x perpendicular al plano, 4=paneles al sol)
-pointing = adcs_select(case_adcs);
 % tipo_panel = 2;           % panel type (1 = galio, 2 = silicio (inutil))                 
 ciclo = 1;                % ciclo de consumos de los componentes (1 = 30s, 2 = 60s) 
 n_fail = 0;               % numero de paneles que fallan (1 = falla 1, 2 = fallan 2, 3 = fallan 3)
@@ -42,6 +41,8 @@ M = 1;                                  % Month of the year March = 1; Feb = 12
 % simulation time and number of points:
 
 npoints = n*T*60/dT + 1;
+
+pointing = adcs_select(case_adcs);
 
 [tspan, theta, Panel, beta, visibility, I, mu] = orbital_main(pointing, n, npoints, T, H, ecc, om, th, M, w, case_adcs, true, true, n_fail);
 
