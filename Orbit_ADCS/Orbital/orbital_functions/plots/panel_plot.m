edce1315e00adb40492ec6e38660203c6afd24bd
Jqazwsx <jmjorgem11@gmail.com>
2022-06-20 04:53:41 +0200

    panel_plot

---
+++ Orbit_ADCS/Orbital/orbital_functions/plots/panel_plot.m
@@ -0,0 +1,37 @@
+function panel_plot(BBx, BBy, BBz, tspan, visibility, T, Panel)
+    figure
+    plot(tspan, BBx)
+    xlim([0,max(tspan)])
+
+    figure
+    plot(tspan, BBy)
+    xlim([0,max(tspan)])
+
+    figure
+    plot(tspan, BBz)
+    xlim([0,max(tspan)])
+    T = T*60;
+
+    figure
+    plot(tspan/T,visibility,'k','LineWidth',1.5)
+    yticks([0 1])
+    ylim([-0.25 1.25])
+    yticklabels({'no','yes'})
+    xl = xlabel('$n_{orbits}$', 'interpreter', 'latex');
+    yl = ylabel('Light', 'interpreter', 'latex');
+    set(get(gca,'ylabel'),'Rotation',0)
+    set(yl, 'Units', 'Normalized', 'Position', [-0.1, 0.91, 0]);
+    set(xl, 'Units', 'Normalized', 'Position', [0.85, -0.075, 0]);
+    box on
+
+
+    figure
+    plot(tspan/T, Panel)
+    xlim([0,max(tspan)/T])
+    xl = xlabel('$n_{orbits}$', 'interpreter', 'latex');
+    yl = ylabel('G $\left[\mathrm{\frac{W}{m^2}}\right]$', 'interpreter', 'latex');
+    set(get(gca,'ylabel'),'Rotation',0)
+    set(yl, 'Units', 'Normalized', 'Position', [-0.1, 0.91, 0]);
+    set(xl, 'Units', 'Normalized', 'Position', [0.85, -0.075, 0]);
+    legend('Panel 1','Panel 2','Panel 3','Panel 4')
+    box on
--- Orbit_ADCS/Orbital/orbital_main.m
+++ Orbit_ADCS/Orbital/orbital_main.m
@@ -95,43 +95,7 @@ theta = linspace(0, n*2*pi, npoints);
 Panel = fail_mode(Panel, n_fail);
 
 if plot_panel == true
-    figure
-    plot(tspan, BBx)
-    xlim([0,max(tspan)])
-
-    figure
-    plot(tspan, BBy)
-    xlim([0,max(tspan)])
-
-    figure
-    plot(tspan, BBz)
-    xlim([0,max(tspan)])
-    T = T*60;
-
-    figure
-    plot(tspan/T,visibility,'k','LineWidth',1.5)
-    yticks([0 1])
-    ylim([-0.25 1.25])
-    yticklabels({'no','yes'})
-    xl = xlabel('$n_{orbits}$', 'interpreter', 'latex','fontsize',18);
-    yl = ylabel('Light', 'interpreter', 'latex','fontsize',18);
-    set(get(gca,'ylabel'),'Rotation',0)
-    set(yl, 'Units', 'Normalized', 'Position', [-0.1, 0.91, 0]);
-    set(xl, 'Units', 'Normalized', 'Position', [0.85, -0.075, 0]);
-    box on
-    set(gca,'FontSize',18)
-
-    figure
-    plot(tspan/T, Panel)
-    xlim([0,max(tspan)/T])
-    xl = xlabel('$n_{orbits}$', 'interpreter', 'latex','fontsize',18);
-    yl = ylabel('G $\left[\mathrm{\frac{W}{m^2}}\right]$', 'interpreter', 'latex','fontsize',18);
-    set(get(gca,'ylabel'),'Rotation',0)
-    set(yl, 'Units', 'Normalized', 'Position', [-0.1, 0.91, 0]);
-    set(xl, 'Units', 'Normalized', 'Position', [0.85, -0.075, 0]);
-    legend('Panel 1','Panel 2','Panel 3','Panel 4')
-    box on
-    set(gca,'FontSize',18)
+    panel_plot(BBx, BBy, BBz, tspan, visibility, T, Panel)
 end
 
 if plot_earthorbit == true
