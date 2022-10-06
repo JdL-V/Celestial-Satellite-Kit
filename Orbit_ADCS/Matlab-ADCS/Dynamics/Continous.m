b28506f2834a17847ace8d7b84dd92b720967b9e
Jqazwsx <jmjorgem11@gmail.com>
2022-10-06 12:22:56 +0200

    translated dynamics (not tested)

---
+++ Orbit_ADCS/Matlab-ADCS/Dynamics/Continous.m
@@ -0,0 +1,55 @@
+function var = getCM(M, R)
+    % Works for position, speed and acceleration
+    CM = zeros(3,1);
+    for j = 1:length(M)
+        CM = CM + M(j).*R(j);
+    end
+    var = CM/sum(M);
+end
+
+function var = getForce(M, ddRc)
+    var = sum(M).*ddRc;
+end
+
+function var = getKineticE(M, dR)
+    dRc = getCM(M, dR)
+    T1 = 0.5*sum(M)*dot(dRc, dRc)
+
+    T2 = 0
+    T = 0
+    for j = 1:length(M)
+        % T = T + 0.5*M(j)*dot(dR(j), dR(j))
+        T2 = T2 + 0.5*M(j)*dot(dR(j) - dRc, dR(j) - dRc)
+    end
+    T = T1 + T2
+    var = [T, T1, T2]
+end
+
+function var = getLinearMom(M, dR)
+    dRc = getCM(M, dR)
+    var = sum(M).*dRc
+end
+
+function var = getAngularMom(M, R, dR, Rp, dRp)
+    H = zeros(3)
+    for j = 1:length(M)
+        H = H + M(j).*cross(R(j) - Rp, dR(j) - dRp)
+    end
+    var = H
+end
+
+function var = getTorque(M, R, Rp, F)
+    Lp = zeros(3,1);
+    for j = 1:length(M)
+        Lp = Lp + cross(R(j) - Rp, F);
+    end
+    var = Lp;
+end
+
+% function var = getInertialtDev(M, R, Rp, F)
+%     Lp = zeros(3)
+%     for j = 1:length(M)
+%         Lp = Lp + cross(R(j) - Rp, F)
+%     end
+%     var = Lp
+% end
---
+++ Orbit_ADCS/Matlab-ADCS/Dynamics/RigidBody.m
@@ -0,0 +1,28 @@
+function var = getInertiaTensor(M, R)
+    Io = zeros(3,3)
+    for j = 1:length(M)
+        Io = Io - M(j).*cross2mat(R)^2
+    end
+    var = I;
+end
+
+function var = translateInertiaFrame(Ig, M, Rp)
+    var = Ig + sum(M)*cross2mat(Rp)*cross2mat(Rp)';
+end
+
+function var = rotateInertiaFrame(Io, dcm)
+    var = dcm*Io*dcm';
+end
+
+% function var = getPrincipalInertia(Io)
+%     dcm = eigvecs(Io)';
+%     Lam, dcm = eigenSort(eigvals(Io), dcm);
+%     % Right handed coordinate frame check
+%     dcm(3,:) = cross(dcm(1,:), dcm(2,:));
+%     var = diagm(Lam), dcm;
+% end
+
+function var = getRotKineticE(Io, om)
+    om = om(:);
+    var = 0.5*om'*Io*om;
+end
