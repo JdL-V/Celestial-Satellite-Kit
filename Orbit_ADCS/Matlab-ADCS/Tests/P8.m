25f0e3e34798a842d8b8fdfc7a6642526181e28c
Jqazwsx <jmjorgem11@gmail.com>
2022-11-03 20:34:47 +0100

    misc

--- Orbit_ADCS/Matlab-ADCS/Tests/P8.m
+++ Orbit_ADCS/Matlab-ADCS/Tests/P8.m
@@ -1,7 +1,9 @@
 IB1 = [1500. 0. -1000.; 0. 2700. 0.; -1000. 0. 3000.];
 [I01, dcm0B1] = getPrincipalInertia(IB1)
 
-IB = [20. -10. -15.; -10. 30. 0.; -15. 0. 15.];
+IB = [20. -10. -15.;
+     -10.  30.  0.; 
+     -15.  0.   15.];
 [I0, dcm0B] = getPrincipalInertia(IB)
 
 T = diag(I0)'
@@ -9,7 +11,7 @@ T = diag(I0)'
 getRotKineticE(IB, [1., 2., 3.])
 getRotKineticE(I0, dcm0B*[1., 2., 3.]')
 
-% 0-2π/dθ  *  0-R/kr^3*r^4 dr  *  0-π/sin^3(ϕ) dϕ pag 478 Bramanti AG2
+% 0-2π// dθ  *  0-R// kr^3*r^4 dr  *  0-π// sin^3(ϕ) dϕ pag 478
 % kR^5/4 
 
 % Ixx = /y^2 + z^2 dm
