d20fdc7edef5cabf7d72c56d6a6fb0a6578e1f44
Jqazwsx <jmjorgem11@gmail.com>
2023-03-21 20:03:21 +0100

    Update getPrincipalInertia.m

--- Orbit_ADCS/Matlab-ADCS/Dynamics/RigidBody/getPrincipalInertia.m
+++ Orbit_ADCS/Matlab-ADCS/Dynamics/RigidBody/getPrincipalInertia.m
@@ -1,6 +1,6 @@
 function [var1, var2] = getPrincipalInertia(Io)
     [dcm, Lam] = eig(Io);
-    [Lam, dcm] = eigenSort(Lam, dcm, 'descend');
+    [Lam, dcm] = eigenSort(Lam, dcm, 'ascend');
     dcm = dcm';
     % Right handed coordinate frame check
     dcm(3,:) = cross(dcm(1,:), dcm(2,:));
