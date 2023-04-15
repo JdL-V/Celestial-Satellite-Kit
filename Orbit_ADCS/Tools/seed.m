8fbf2b9deac89794e6093fca6755850ee7fb38f6
JdL-V <jmjorgem11@gmail.com>
2023-04-15 22:55:38 +0200

    Update seed.m

--- Orbit_ADCS/Tools/seed.m
+++ Orbit_ADCS/Tools/seed.m
@@ -1,5 +1,5 @@
 function seed(n)
-    if n >= 2^32 || n < 0 || mod(n,1) ~= 0
+    if ~isnumeric(n) || n >= 2^32 || n < 0 || mod(n,1) ~= 0
         warning('Seed randomized due to invalid number')
         n = randi([0, 2^32-1]);
     end
