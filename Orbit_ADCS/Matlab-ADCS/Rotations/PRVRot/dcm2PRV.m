61ac8750446467dbfbc23557ad2b65cee2cf633c
JdL-V <jmjorgem11@gmail.com>
2023-04-23 22:27:48 +0200

    fix dcm2PRV not giving normalized vector

--- Orbit_ADCS/Matlab-ADCS/Rotations/PRVRot/dcm2PRV.m
+++ Orbit_ADCS/Matlab-ADCS/Rotations/PRVRot/dcm2PRV.m
@@ -11,7 +11,7 @@ function var = dcm2PRV(dcm)
     end
 
     Angle = acos(0.5*(Mat(1,1) + Mat(2,2) + Mat(3,3) - 1));
-    x = 1/(2*sin(Angle)).*[Mat(2,3)-Mat(3,2), Mat(3,1)-Mat(1,3), Mat(1,2)-Mat(2,1)];
+    x = normalize(1/(2*sin(Angle)).*[Mat(2,3)-Mat(3,2), Mat(3,1)-Mat(1,3), Mat(1,2)-Mat(2,1)], 'norm');
     prv = PRV(Angle, x, outFrame, inFrame);
     var = prv;
 end
